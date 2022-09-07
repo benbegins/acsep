799925121f4f10b794a80db0e796b574d6404771
benbegins <62623777+benbegins@users.noreply.github.com>
2022-09-07 16:43:40 +0200

    init

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+	content: ["./**/*.php", "./**/*.twig"],
+	theme: {
+		colors: {
+			dark: "#00282a",
+			light: "#f4eceb",
+		},
+		container: {
+			padding: {
+				DEFAULT: "1.5rem",
+				sm: "3rem",
+				lg: "4vw",
+			},
+			center: true,
+		},
+		screens: {
+			sm: "640px",
+			md: "768px",
+			lg: "1024px",
+			xl: "1280px",
+			xxl: "1480px",
+		},
+	},
+	variants: {},
+	plugins: [],
+}
