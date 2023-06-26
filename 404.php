ef6dbd163746712f87bbaac4d4acc00372561200
benbegins <62623777+benbegins@users.noreply.github.com>
2023-06-26 17:15:44 +0200

    404

--- 404.php
+++ 404.php
@@ -9,13 +9,27 @@ get_header();
             <div class="container pt-6 lg:pt-section flex flex-col-reverse lg:flex-row lg:items-center">
 
                 <div class="lg:w-7/12">
-                    <img class="rounded-xl w-full" src="<?= get_template_directory_uri(  ); ?>/src/img/photos/404.jpg" alt="404">
+                    <img class="rounded-xl w-full saturate-50 opacity-75" src="<?= get_template_directory_uri(  ); ?>/src/img/photos/404.gif" alt="404">
                 </div>
                 
                 <div class="text-center mb-12 lg:w-5/12 lg:px-12 lg:mb-0">
                     <h1 class="font-extrabold text-[8rem] leading-none lg:text-[13vw]">404</h1>
                     <p class="text-lg my-6 lg:w-2/3 lg:mx-auto"><?php _e('Oups ! La page que vous avez demandée n’existe pas…', 'acsep'); ?></p>
-                    <a href="<?= pll_home_url( ); ?>" class="btn-secondary"><?php _e('Retour à la page d’accueil', 'acsep'); ?></a>
+                    <div><a href="<?= pll_home_url( ); ?>" class="btn-secondary"><?php _e('Retour à la page d’accueil', 'acsep'); ?></a></div>
+                    <?php 
+                    $args = array(
+                        'post_type'=>'solutions',
+                        'name'=>'wms-izypro'
+                    );
+                    $izypro = get_posts( $args )[0];
+                    // Check if post exist in current language
+                    if(pll_get_post($izypro->ID)):
+                    ?>
+                    <div class="mt-3"><a href="<?= get_the_permalink(pll_get_post($izypro->ID)); ?>" class="btn-secondary"><?php _e('Découvrez notre WMS IzyPro', 'acsep'); ?></a></div>
+                    <?php endif; ?>
+                    <div class="mt-3">
+                        <a class="btn-secondary" href="<?php echo get_post_type_archive_link( 'services' ); ?>"><?php _e('Découvrez nos services', 'acsep'); ?></a>
+                    </div>
                 </div>
 
 
