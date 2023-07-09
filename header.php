06617b75facf84f82e1c197daef03b9fe2ecf258
benbegins <62623777+benbegins@users.noreply.github.com>
2023-07-09 14:44:13 +0200

    ga

--- header.php
+++ header.php
@@ -3,21 +3,19 @@
 <head>
     <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
+
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-42EMWEW3K4"></script>
+    <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'G-42EMWEW3K4');
+    </script>
     
     <?php wp_head(); ?>
 </head>
-
-
-<!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-42EMWEW3K4"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-42EMWEW3K4');
-</script>
-
 
 <body <?php body_class(); ?>>
 <?php wp_body_open(); ?>
