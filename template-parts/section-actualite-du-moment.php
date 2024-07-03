7288ffe3821bdffde8c543c19e355906d6f88048
benbegins <62623777+benbegins@users.noreply.github.com>
2024-07-03 10:48:01 +0200

    fix image size on news section

--- template-parts/section-actualite-du-moment.php
+++ template-parts/section-actualite-du-moment.php
@@ -12,8 +12,8 @@
 <div class="pb-section-mobile lg:pb-section">
     <section class="section-actualite-du-moment relative pb-section-mobile md:pb-0 text-light">
         <div class="container">
-            <div class="section-actualite-du-moment__content relative md:grid md:grid-cols-2 md:gap-8 md:rounded-2xl">
-                <div class="relative -top-5 rounded-xl md:rounded-2xl overflow-hidden md:left-5"><?php echo wp_get_attachment_image($featured_article_image_id, 'medium_large', false, array('class'=>'w-full md:h-full md:absolute md:object-cover')); ?></div>
+            <div class="section-actualite-du-moment__content relative md:grid md:grid-cols-2 md:gap-8 md:rounded-2xl md:items-center">
+                <div class="relative -top-5 rounded-xl md:rounded-2xl overflow-hidden md:left-5 md:min-h-[30vw] md:h-full"><?php echo wp_get_attachment_image($featured_article_image_id, 'medium_large', false, array('class'=>'w-full md:h-full md:absolute md:object-cover')); ?></div>
                 <div class="mt-6 md:mt-0 flex flex-col items-start gap-2 md:py-section md:px-8 xl:w-2/3 xl:mx-auto xl:px-0">
                     <h2 class="lg:text-lg font-extrabold leading-tight"><?php _e('Actualité du moment', 'acsep'); ?></h2>
                     <h3 class="text-xl lg:text-2xl font-extrabold leading-tight"><a href="<?php echo $featured_article_link; ?>"><?php echo $featured_article_title ?></a></h3>
